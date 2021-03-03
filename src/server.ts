1214402e7ec80f9b7e7e8290b9754ca355cd499a
Félix Vicente <felixvicent1306@gmail.com>
2021-03-03 15:37:47 -0300

    Segundo  dia

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import './database';
+
+import { router } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.send("Hello Felps");
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log('Server is running'));
